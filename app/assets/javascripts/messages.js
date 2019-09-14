db2b3627ab20e9bf66b69963a8579f00c4c791a6
kaito-s <kaito.2019.0802@gmail.com>
2019-09-14 16:34:49 +0900

    image change

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,6 @@
 $(function() {
     function buildMessage(message) {
+    var image = "";
     var image = message.image ? `<img src="${message.image}">` : "";
       var html =  `<div class="main__chat__box" data-id="${message.id}">
                       <div class="main__chat__box--name">
@@ -11,16 +12,17 @@ $(function() {
                       <div class="main__chat__box--message">
                         ${message.content}
                       </div>
-                      <div class="main__chat__box--image">
-                        ${image} 
-                      </div>
-                    </div>`
+                  </div>
+                  <div class="main__chat__box--image">
+                    ${image} 
+                  </div>`
+                  
       return html;
     }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -42,28 +44,32 @@ $(function() {
     $('.main__form__box__btn').prop('disabled', false);
   })
   })
+  
   var reloadMessages = function() {
     if(window.location.href.match(/\/groups\/\d+\/messages/)) {
     
-    last_message_id = $('.main__chat__box').last().data('id')
+    var last_message_id = $('.main__chat__box:last').data('id')
+    console.log(last_message_id);
       
     $.ajax({
       url: 'api/messages',
-      type: 'get',
+      type: 'GET',
       dataType: 'json',
       data: {id: last_message_id}
+      
         })
     .done(function(messages) {
       var insertHTML = '';
         messages.forEach(function(message){
-        var html = buildMessage(message);
-        $('.main__chat').append(html);
+        insertHTML = buildMessage(message);
+        $('.main__chat').append(insertHTML);
         
         $("form")[0].reset();
         $('.main__chat').animate({ scrollTop: $('.main__chat')[0].scrollHeight});
         });
       })
       };
-  }
-setInterval(reloadMessages, 5000);
+      
+}
+  setInterval(reloadMessages, 5000);
 });
